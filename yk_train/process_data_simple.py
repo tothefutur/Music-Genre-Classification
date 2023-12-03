59f79a30c18f2037b143892c845f0ad8d9a5f2ae
Yang Ke <221900275@smail.nju.edu.cn>
2023-12-03 19:22:32 +0800

    新建 GTZANDataset_scale
    1、在全连接层情况下50epoch 准确率可达90%
    2、resize功能还没写好，暂时不能用卷积层训练，留待解决

--- yk_train/process_data_simple.py
+++ yk_train/process_data_simple.py
@@ -1,6 +1,7 @@
 import torch
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
 from torch.utils.data import Dataset, random_split
 import sklearn.preprocessing as skp
 import sklearn.model_selection as skms
@@ -102,7 +103,7 @@ class my_dataset_more_features(Dataset):
             single_data = torch.Tensor(single_data)
             single_data = single_data.view(1, self.num_features)
             # 将(1, 40)形状数据转化为(1, resize, resize)形状数据
-            # 创建一个全为 0 的张量，其形状为 (1, 224-40)
+            # 创建一个全为 0 的张量，其形状为 (1, resize-40)
             zeros = torch.zeros(1, resize - self.num_features)
             # 将 zeros 连接到 single_data 上，以将其形状改变为 (1, 224)
             single_data = torch.cat((single_data, zeros), dim=1)
@@ -142,6 +143,59 @@ class GTZANDataset_more_features:
         elif train == "False":
             return self.testDataset
 
+class GTZANDataset_scale:
+    def __init__(self, rootDir=r"..\..\dataset\archive\Data\features_3_sec.csv", resize=40):
+        df = pd.read_csv(rootDir)
+        df = df.drop(labels="filename", axis=1)
+        # Feature Extraction
+        class_list = df.iloc[:, -1]
+        converter = skp.LabelEncoder()
+        y = converter.fit_transform(class_list)
+
+        # Scale the features
+        scaler = skp.StandardScaler()
+        X = scaler.fit_transform(np.array(df.iloc[:, :-1], dtype=float))
+        # resize X to (len(X), resize, resize), 多出来的部分用0填充
+        X = torch.tensor(X)
+        X = X.view(len(X), 1, -1)
+        for i in range(len(X)):
+            """
+            Traceback (most recent call last):
+            File "D:\yk\Study\2023fall\IntroToAI\project\example\src\yk_train\process_data_simple.py", line 190, in <module>
+            dataset = GTZANDataset_scale(r"D:\yk\Study\2023fall\IntroToAI\project\example\dataset\archive\Data\features_3_sec.csv", resize=224)
+            File "D:\yk\Study\2023fall\IntroToAI\project\example\src\yk_train\process_data_simple.py", line 165, in __init__
+            X[i] = torch.cat((X[i], zeros), dim=1)
+            RuntimeError: The expanded size of the tensor (58) must match the existing size (224) at non-singleton dimension 1.  Target sizes: [1, 58].  Tensor sizes: [224]
+            未知错误，暂时不知道怎么解决
+            """
+
+            zeros = torch.zeros(1, resize - X.shape[2])
+            print(X[i].shape)
+            print(zeros.shape)
+            X[i] = torch.cat((X[i], zeros), dim=1)
+            X[i] = X[i].view(1, resize, 1)
+            X[i] = X[i].repeat(1, 1, resize)
+
+        # Split the dataset
+        X_train, X_test, y_train, y_test = skms.train_test_split(X, y, test_size=0.3, random_state=0)
+        X_train = torch.Tensor(X_train)
+        X_test = torch.Tensor(X_test)
+        y_train = torch.Tensor(y_train)
+        y_test = torch.Tensor(y_test)
+        self.trainDataset = torch.utils.data.TensorDataset(X_train, y_train)
+        self.testDataset = torch.utils.data.TensorDataset(X_test, y_test)
+
+    def __call__(self, train="False"):
+        """
+        :param train:
+        :return: dataset
+        """
+        if train == "True":
+            return self.trainDataset
+        elif train == "False":
+            return self.testDataset
+
+
 if __name__ == "__main__":
-    dataset = GTZANDataset_more_features(r"..\dataset\archive\Data\features_3_sec.csv", resize=224)
-    print(dataset.data[0][0])
+    dataset = GTZANDataset_scale(r"D:\yk\Study\2023fall\IntroToAI\project\example\dataset\archive\Data\features_3_sec.csv", resize=224)
+    print(dataset.trainDataset[0][0].shape)
